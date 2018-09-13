df4dd11c9975e2a44ba411cd563c2ce181f866b8
Atmospheric_b-10 <atmobsod@gmail.com>
2018-09-13 22:34:40 +0300

    changed from local to mongoLab

--- server/server.js
+++ server/server.js
@@ -8,13 +8,17 @@ const schema = require("./schema/schema");
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = "mongodb://localhost/lyricaldb";
+// const MONGO_URI = "mongodb://localhost/lyricaldb";
+const MONGO_URI = 'mongodb://admin:xgEH]n45DpVt@ds157422.mlab.com:57422/lyricaldb';
+
 if (!MONGO_URI) {
   throw new Error("You must provide a MongoLab URI");
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URI);
+mongoose.connect(MONGO_URI, {
+  useMongoClient: true,
+});
 mongoose.connection
   .once("open", () => console.log("Connected to MongoLab instance."))
   .on("error", error => console.log("Error connecting to MongoLab:", error));
